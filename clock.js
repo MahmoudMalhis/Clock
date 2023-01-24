98d3a3e191df9c4ff99d451c92c32c9596acb0e3
MahmoudMalhis <112867127+MahmoudMalhis@users.noreply.github.com>
2023-01-24 11:37:05 +0200

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,26 @@
+let clock = document.querySelector(".clock");
+
+function time() {
+  let date = new Date();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let session = "AM";
+
+  if (hour === 0) {
+    hour = 12;
+  } else if (hour > 12) {
+    hour -= 12;
+    session = "PM";
+  }
+  hour = hour < 10 ? "0" + hour : hour;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  let timeNaw = `${hour}:${minutes}:${seconds} ${session}`;
+  console.log(timeNaw);
+  clock.innerHTML = timeNaw;
+  setInterval(time, 1000);
+}
+
+time();
